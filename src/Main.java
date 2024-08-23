43ed5318df50862e5976fa8072c0063275b37cb7
Alfsn49 <edgar.amanta@espoch.edu.ec>
2024-08-22 21:28:19 -0500

    Clase 1 Tipos de datos y declaración de variables

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import practica1.Variable;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Main mainInstance = new Main();
+        Variable var = new Variable();
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.print("Hello and welcome! \n");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
+            System.out.println("i = " + i);
+
+        }
+
+        int a =10, b=10;
+        int si = mainInstance.suma(a,b);
+        System.out.println(si);
+        System.out.println(var.Variable());
+
+    }
+
+    public int suma(Integer a, Integer b){
+        int c;
+        c = a + b;
+        return c;
+    }
+}
---
+++ src/practica1/Variable.java
@@ -0,0 +1,15 @@
+package practica1;
+
+public class Variable {
+    int numero =0;
+    float decimal = 10.3023F;
+    double doble = 10.20;
+    char letra = 'A';
+    String palabra = "Hola";
+    boolean bandera = true;
+    long extenso = 1233443534;
+
+    public String Variable() {
+        return "Hola desde la variable clase";
+    }
+}
