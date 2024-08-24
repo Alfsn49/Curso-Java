e3dbf8d432814b870d55ac70da9800ea21e8cfbe
Alfsn49 <edgar.amanta@espoch.edu.ec>
2024-08-24 11:36:30 -0500

    Clase 2 Operaciones en Java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,12 @@
 import practica1.Variable;
+import practica1.Ejerciciio;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Main mainInstance = new Main();
         Variable var = new Variable();
+        Ejerciciio ej = new Ejerciciio();
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.print("Hello and welcome! \n");
@@ -21,6 +23,12 @@ public class Main {
         int si = mainInstance.suma(a,b);
         System.out.println(si);
         System.out.println(var.Variable());
+
+        System.out.print("Clase 2 \n");
+        System.out.print(var.aritmetica());
+        System.out.print("\n");
+        System.out.print("Ejercicio Practico \n");
+        ej.ej1();
 
     }
 
--- src/practica1/Variable.java
+++ src/practica1/Variable.java
@@ -1,7 +1,7 @@
 package practica1;
 
 public class Variable {
-    int numero =0;
+    int numero = 0;
     float decimal = 10.3023F;
     double doble = 10.20;
     char letra = 'A';
@@ -11,5 +11,14 @@ public class Variable {
 
     public String Variable() {
         return "Hola desde la variable clase";
+    }
+
+    public int aritmetica(){
+        int n1, n2, total;
+
+        n1= 2; n2=2;
+        total = n1 + n2;
+
+        return total;
     }
 }
